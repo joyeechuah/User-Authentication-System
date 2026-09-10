5a4b41c2fd982b8972d2fc19d04c2431ebc0fe9c
Chuah Jo Yee <chuahjytaeyoon@gmail.com>
2026-09-10 14:54:29 +0800

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS login_logout_auth;
+
+USE login_logout_auth;
+
+CREATE TABLE IF NOT EXISTS user (
+    name             VARCHAR(255) NOT NULL,
+    email            VARCHAR(75)  NOT NULL UNIQUE,
+    password         VARCHAR(255) NOT NULL,
+    confirm_password VARCHAR(255) NOT NULL
+    );
