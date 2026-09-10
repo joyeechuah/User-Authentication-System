570d097aad3d6a6882b1c0b93402a5d49afd966d
Chuah Jo Yee <chuahjytaeyoon@gmail.com>
2026-09-10 14:57:00 +0800

    Add primary key to user table in database.sql

--- database.sql
+++ database.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS login_logout_auth;
 USE login_logout_auth;
 
 CREATE TABLE IF NOT EXISTS user (
+	id               INT AUTO_INCREMENT PRIMARY KEY,
     name             VARCHAR(255) NOT NULL,
     email            VARCHAR(75)  NOT NULL UNIQUE,
     password         VARCHAR(255) NOT NULL,
