6fca15e8e097bbaef62be610e02471a38678d6a5
Chuah Jo Yee <chuahjytaeyoon@gmail.com>
2026-09-12 11:17:41 +0800

    Modify user table structure in database.sql
    Removed confirm_password field from user table.

--- database.sql
+++ database.sql
@@ -1,11 +1,10 @@
 CREATE DATABASE IF NOT EXISTS login_logout_auth;
-
+ 
 USE login_logout_auth;
-
+ 
 CREATE TABLE IF NOT EXISTS user (
-	id               INT AUTO_INCREMENT PRIMARY KEY,
-    name             VARCHAR(255) NOT NULL,
-    email            VARCHAR(75)  NOT NULL UNIQUE,
-    password         VARCHAR(255) NOT NULL,
-    confirm_password VARCHAR(255) NOT NULL
-    );
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(75)  NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
